0ebaba7bee01c1f72030b97755649d583cdba73d
Dave <dave@Mac-Studio-192.local>
2024-03-26 17:40:07 +0100

    RAG IS WORKING

--- extractUrls.py
+++ extractUrls.py
@@ -92,12 +92,38 @@ vectorstore = Chroma.from_documents(
 
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
 
-from langchain_openai import ChatOpenAI
+# CHOOSE LLM
+# from langchain_openai import ChatOpenAI
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo-0125", temperature=0)
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo-0125", temperature=0)
 
+from langchain_community.llms import Ollama
+
+llm = Ollama(model="qwen:72b")
+
+# llm.invoke("Tell me a joke")
+
+
+# IMPORT PROMPT 
 from langchain import hub
 
 prompt = hub.pull("rlm/rag-prompt")
 
-prompt
+# IMPLEMENT RAG
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+for chunk in rag_chain.stream("Qui a cree ce site?"):
+    print(chunk, end="", flush=True)
